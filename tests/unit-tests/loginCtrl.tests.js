4509d7e7d2edeec85324295b1827a6cc263668d5
Cassie Schmitz <schmitzc@gmail.com>
2015-12-03 10:04:59 -0800

    Add passing unit tests!

--- tests/unit-tests/loginCtrl.tests.js
+++ tests/unit-tests/loginCtrl.tests.js
@@ -1,35 +1,66 @@
 describe('loginCtrl', function() {
   var controller,
-    deferredLogin,
-    dinnersMock,
+    scope,
+    ionicPopupMock,
     stateMock,
-    ionicPopupMock;
+    deferredLogin,
+    dinnersMock;
 
   beforeEach(module('app'));
 
+  // disable template caching
+  beforeEach(module(function($provide, $urlRouterProvider) {
+    $provide.value('$ionicTemplateCache', function() {});
+    $urlRouterProvider.deferIntercept();
+  }));
+
+  beforeEach(inject(function($controller, $q, $rootScope) {
+    scope = $rootScope.$new();
+
+    ionicPopupMock = jasmine.createSpyObj('$ionicPopup spy', ['alert']);
+
+    stateMock = jasmine.createSpyObj('$state spy', ['go']);
+
+    deferredLogin = $q.defer();
+
+    dinnersMock = {
+      login: jasmine.createSpy('login spy')
+        .and.returnValue(deferredLogin.promise)
+    };
+
+    $controller('loginCtrl', {
+      '$scope': scope,
+      '$ionicPopup': ionicPopupMock,
+      '$state': stateMock,
+      'Dinners': dinnersMock
+    });
+  }));
+
   describe('#doLogin', function() {
-    beforeEach(inject(function($controller, $q, _$scope_) {
-      deferredLogin = $q.defer();
-
-      dinnersMock = {
-        login: jasmine.createSpy('login spy')
-          .and.returnValue(deferredLogin.promise)
-      };
-
-      stateMock = jasmine.createSpyObj('$state spy', ['go']);
-
-      ionicPopupMock = jasmine.createSpyObj('$ionicPopup spy', ['alert']);
-
-      controller = $controller('loginCtrl', {
-        '$ionicPopup': ionicPopupMock,
-        '$state': stateMock,
-        'Dinners': dinnersMock,
-        '$scope': _$scope_
-      });
-    }));
+    beforeEach(function () {
+      scope.username = 'test1';
+      scope.password = 'password1';
+      scope.doLogin();
+    });
 
     it('should call login on Dinners', function() {
       expect(dinnersMock.login).toHaveBeenCalledWith('test1', 'password1');
+    });
+
+    describe('when the login is executed', function() {
+      it('should change state to my-dinners if successful', function() {
+        deferredLogin.resolve();
+        scope.$digest();
+
+        expect(stateMock.go).toHaveBeenCalledWith('my-dinners');
+      });
+
+      it('should show a popup if unsuccessful', function() {
+        deferredLogin.reject();
+        scope.$digest();
+
+        expect(ionicPopupMock.alert).toHaveBeenCalled();
+      });
     });
   });
 });
